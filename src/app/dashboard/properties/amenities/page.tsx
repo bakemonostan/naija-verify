dc71982168b1616238846e6a875c1fc11fe47096
parsepec <parsepecq@gmail.com>
2023-08-04 12:14:19 +0100

    Fix: property modal

--- src/app/dashboard/properties/amenities/page.tsx
+++ src/app/dashboard/properties/amenities/page.tsx
@@ -1,16 +1,20 @@
 'use client'
-import { useState } from 'react';
 import Link from "next/link"
 import PropertyListedIcon from "@/components/Icons/PropertyListedIcon"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation";
+import {
+    Dialog,
+    DialogContent,
 
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 function PropertyModal() {
     return (
-        <div className="relative">
-            <div className="absolute bg-white rounded-md text-center flex flex-col justify-center px-5 py-10">
+        <div className={`self-center my-auto relative`}>
+            <div className="bg-white rounded-md text-center flex flex-col justify-center px-5 py-10">
                 <div className="mx-auto"><PropertyListedIcon width="100" height="100" /></div>
                 <h2 className="text-[22px] font-semibold font-prompt">Property Listed<br /> Successfully!</h2>
                 <p className="font-rubik text-gray-500 text-xs my-2">Your property has been listed successfully! You can now start adding tenant</p>
@@ -23,13 +27,13 @@ function PropertyModal() {
 
 export default function Amenities() {
     const router = useRouter();
-
     return (
         <div className="flex flex-col justify-center justify-items-center w-12/12 mx-auto">
             <div className='py-6'>
                 <h2 className='text-[26px] font-semibold font-prompt '>Amenities and Furnishing</h2>
                 <p className='text-gray-400'>You can change this information anytime</p>
             </div>
+
             <div className='flex flex-col justify-center self-center w-full font-rubik'>
                 <h3 className="text-sm">SELECT ALL THAT APPLIES</h3>
                 <div className="py-8">
@@ -118,10 +122,17 @@ export default function Amenities() {
                     </div>
                 </div>
                 <div className='text-center py-10'>
-                    <div className='py-2'><Link href="/dashboard/properties/amenities"><Button>Next</Button> </Link></div>
-                    <div className='py-2'><button className='text-primary border-primary border font-bold px-24 py-3 w-full font-rubik' onClick={() => { router.push('/dashboard/properties/utilities') }}>Back</button></div>
+                    <Dialog>
+                        <DialogTrigger className='w-full'><div className='py-2'><Button>Next</Button></div>
+                        </DialogTrigger>
+                        <div className='py-2'><button className='text-primary border-primary border font-bold px-24 py-3 w-full font-rubik' onClick={() => { router.push('/dashboard/properties/utilities') }}>Back</button></div>
+
+                        <DialogContent className='mt-12 w-10/12'>
+                            <PropertyModal />
+                        </DialogContent>
+                    </Dialog>
                 </div>
-                <PropertyModal />
+
             </div>
         </div>
     )
