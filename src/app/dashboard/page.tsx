d6ac74bb2b27ff0965e883e73728a283084730f6
bakemonostan <ehizomokhomion@gmail.com>
2023-08-18 10:01:42 +0100

    remove tempredirect

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
-import TempRedirect from "../TempRedirect";
+// import TempRedirect from "../TempRedirect";
 
 export default function Dashboard() {
     return (
-
         <div className="py-4">
-            <TempRedirect />
+            {/* <TempRedirect /> */}
             <h3 className="text-2xl">Hello, Oladotun 👋</h3>
             <p className="font-[300] text-sm">Good morning! Welcome to your dashboard</p>
         </div>
